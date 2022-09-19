7e872516e660bc6a5975cba04b4912e6870f665b
Alex Brian Naughton <alexbriannaughton@gmail.com>
2022-09-19 14:45:20 -0700

    commit tree 6ee7e2d1c584b5948670f0ddf8a3c0f56197d169

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    if (pickupLocation <= 42) {
+        return 42 - pickupLocation
+    } else {
+        return pickupLocation - 42
+    }
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation)*264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return start*264 - destination*264
+    } else {
+        return destination*264 - start*264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400){
+        return 0;
+    } else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400)*.02;
+    } else if (distanceFromHqInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25;
+    } else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return 'cannot travel that far';
+    }
+}
